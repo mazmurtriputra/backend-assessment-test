409dd03a5c962b67f8fdafb0ad83299771357ba6
mazmurtriputra <mazmurtriputra99@gmail.com>
2024-09-04 02:38:05 +0000

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DebitCardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('debit-cards', [DebitCardController::class, 'index']);
--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Models\DebitCard;
 use App\Models\User;
+use Database\Factories\DebitCardTransactionFactory;
+use App\Models\DebitCardTransaction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -27,7 +29,19 @@ class DebitCardTransactionControllerTest extends TestCase
 
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
-        // get /debit-card-transactions
+        $customer = User::factory()->create();
+        $debitCard = DebitCard::factory()->create(['user_id' => $customer->id]);
+        DebitCardTransactionFactory::factory()->count(3)->create([
+            'debit_card_id' => $debitCard->id,
+            'amount' => 1000,
+            'currency_code' => 'USD',
+        ]);
+    
+        $response = $this->actingAs($customer)->get('api/debit-card-transactions');
+    
+        $response->assertStatus(200)
+                 ->assertJsonCount(3) // assuming JSON response with a count of 3 transactions
+                 ->assertJsonFragment(['amount' => 1000, 'currency_code' => 'USD']);
     }
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
